2702d2f99c21e4acde1627ca84c586931693984c
khaoula bachiri <khaoulabachiri1992@gmail.com>
2018-06-02 08:18:25 +0200

    liste des Consultation

--- src/main/java/com/eheio/hello/web/HelloController.java
+++ src/main/java/com/eheio/hello/web/HelloController.java
@@ -1,7 +1,9 @@
 package com.eheio.hello.web;
 
+import com.eheio.hello.Model.Consultation;
 import com.eheio.hello.Model.Maladie;
 import com.eheio.hello.Model.Personne;
+import com.eheio.hello.Service.Serviceimpl.ConsultationServiceimpl;
 import com.eheio.hello.Service.Serviceimpl.MaladieServiceimpl;
 import com.eheio.hello.Service.Serviceimpl.PersonneServiceimpl;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -25,6 +27,11 @@ public class HelloController {
     @Autowired
     public void setMaladie(Maladie maladie) { this.maladie = maladie; }
     @Autowired
+    public void setConsultation(Consultation consultation) {
+        this.consultation = consultation;
+    }
+
+    @Autowired
     public void setPersonneServiceimpl(PersonneServiceimpl personneServiceimpl) {
         this.personneServiceimpl = personneServiceimpl;
     }
@@ -32,11 +39,17 @@ public class HelloController {
     public void setMaladieServiceimpl(MaladieServiceimpl maladieServiceimpl) {
         this.maladieServiceimpl = maladieServiceimpl;
     }
+    @Autowired
+    public void setConsultationServiceimpl(ConsultationServiceimpl consultationServiceimpl) {
+        this.consultationServiceimpl = consultationServiceimpl;
+    }
     Personne personne;
     Maladie maladie;
+    Consultation consultation;
 
     MaladieServiceimpl maladieServiceimpl;
     PersonneServiceimpl personneServiceimpl ;
+    ConsultationServiceimpl consultationServiceimpl;
     @RequestMapping(value = "/login", method = RequestMethod.GET)
     public String hello() {
         return "hello";
@@ -45,27 +58,36 @@ public class HelloController {
     @RequestMapping(value = "/login", method = RequestMethod.POST)
     public ModelAndView hello(@RequestParam(value="username") String username,
                               @RequestParam(value="password") String password , Model model) {
+
         ModelAndView m = new ModelAndView();
         m.setViewName("Home");
         String user ="";
         String role="";
+        String nom ="khaoula";
         List<Personne> list = new ArrayList<Personne>();
         List<Maladie> listm = new ArrayList<Maladie>();
+        List<Consultation> listC =new ArrayList<Consultation>();
         personne.setLogin(username);
         personne.setPasswrd(password);
+
         for (Personne p : personneServiceimpl.getPersonne())
         {
             if(personne.getLogin().equals(p.getLogin()) && personne.getPasswrd().equals(p.getPasswrd()))
             {
-                user =p.getNom();
-                role =p.getRole();
-                m.addObject("user", user);
-                m.addObject("role",role);
+
+                    user =p.getNom();
+                    role =p.getRole();
+                    m.addObject("user", user);
+                    m.addObject("role",role);
+
+
             }
+
         }
         for (Personne o : personneServiceimpl.getPersonne())
         {
             list.add(o);
+            m.addObject("nom",nom);
             m.addObject("list", list);
 
         }
@@ -73,8 +95,14 @@ public class HelloController {
         for (Maladie maladie : maladieServiceimpl.getMaldie())
         {
             listm.add(maladie);
-            System.out.println(m.toString());
             m.addObject("listm", listm);
+
+        }
+        for(Consultation consultation: consultationServiceimpl.getConsultations())
+        {
+            listC.add(consultation);
+            m.addObject("listc", listC);
+            System.out.println(m.toString());
 
         }
 
